c23480d885675b475d5dea7adac1143d6336b511
gicziunicorn <giczidani100@gmail.com>
2026-03-16 11:21:38 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const cursordiv = document.getElementById("kurzor");
-
-window.addEventListener("mousemove", (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    cursordiv.style.left = `${x - 12.5}px`;
-    cursordiv.style.top = `${y - 12.5}px`;
-})
